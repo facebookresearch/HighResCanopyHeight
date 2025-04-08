52469987458acb0c34233d0f1527a95c15dab9cd
Jamie Tolan <jetolan@meta.com>
2025-04-08 08:22:28 -0700

    add inline license specification

--- src/raster_utils.py
+++ src/raster_utils.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the Apache License, Version 2.0
+# found in the LICENSE file in the root directory of this source tree.
+
 import numpy as np
 from skimage.util import view_as_windows
 from scipy.ndimage import gaussian_filter
